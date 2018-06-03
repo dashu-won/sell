d9cc3a5fbe48714875c533d20194426cb0d0b2ec
dashu-won <dashu.won@outlook.com>
2018-06-04 00:38:59 +0800

    add new

---
+++ src/page/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>这是一个home页面</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      guessCity: '', // 当前城市
+      guessCityid: '', // 当前城市id
+      hotcity: [], // 热门城市列表
+      groupcity: {} // 所有城市列表
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
