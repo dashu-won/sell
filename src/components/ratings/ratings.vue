c67f6fe4fcaa988750f175e598a330d36fead2e1
dashu <dashu.won@outlook.com>
2018-06-22 12:29:42 +0800

    Add comment data

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -3,24 +3,24 @@
     <div class="ratings-content">
       <div class="overview">
         <div class="overview-left">
-          <h1 class="score"></h1>
+          <h1 class="score">{{seller.score}}</h1>
           <div class="title">综合评分</div>
-          <div class="rank">高于周边商家%</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}%</div>
         </div>
         <div class="overview-right">
           <div class="score-wrapper">
             <span class="title">服务态度</span>
-            <star :size="36"></star>
-            <span class="score"></span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
           </div>
           <div class="score-wrapper">
             <span class="title">商品评分</span>
-            <star :size="36"></star>
-            <span class="score"></span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
           </div>
           <div class="delivery-wrapper">
             <span class="title">送达时间</span>
-            <span class="delivery">分钟</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
           </div>
         </div>
       </div>
